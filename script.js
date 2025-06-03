e6a45ce444d258b17c49a291fb3f23738dedf325
Anton Bahrul Hasan <antonbahrul225@gmail.com>
2025-06-03 09:10:50 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Animasi saat elemen .member muncul di layar
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+document.querySelectorAll('.member').forEach(member => {
+  observer.observe(member);
+});
